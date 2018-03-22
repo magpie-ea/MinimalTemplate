f0fad9dfeda5ba8bf16f309a1efaddf1076f1fe2
Michael Franke <the.secret.goldfish@me.com>
2018-03-22 12:10:31 +0100

    moved showNextView from helpers.js to main.js

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,30 +1,3 @@
-// attaches exp.findNextView() function to all the buttons that bring
-// the next view when clicked. Which view should be shown is determined by 
-// the conditionals in exp.findNextView() which is located in main.js
-// if the button has id='send-data' (the button in subj info template has it),
-// the data is collected sent before exp.findNextView(); is called
-var showNextView = function() {
-    var nexts = $('.next-view');
-    for (var i=0; i<nexts.length; i++){
-	if (nexts[i].id === 'sends-data') {
-	    nexts[i].addEventListener('click', function() {
-		for (var i=0; i<exp.data.out.length; i++) {
-		    exp.data.out[i].age = $('#age').val(),
-		    exp.data.out[i].gender = $('#gender').val(),
-		    exp.data.out[i].education = $('#education').val(),
-		    exp.data.out[i].languages = $('#languages').val(),
-		    exp.data.out[i].comments = $('#comments').val().trim()
-		}
-		exp.findNextView();
-	    });
-	} else {
-	    nexts[i].addEventListener('click', function() {
-		exp.findNextView();
-	    });
-	}
-    }
-};
-
 // submits to MTurk's servers if config.is_MTurk is set to true
 // and the correct url is given in config.MTurk_server
 var submitToMTurk = function() {
