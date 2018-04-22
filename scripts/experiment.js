009f04cbae58dc36f7ba03707a3048d26fa6f5ef
JI Xiang <hi@xiangji.me>
2018-04-23 01:10:53 +0200

    Enable feeding the trial information as a CSV file (#7)
    Might need to put this functionality into a library as well, together with submission.

--- scripts/experiment.js
+++ scripts/experiment.js
@@ -1,17 +1,17 @@
 // initialize the experiment
 exp.init = function() {
-    
+
     // allocate storage room for global and trial data
     this.global_data = {};
     this.trial_data = [];
-    
+
     // record current date and time
     this.global_data.startDate = Date();
     this.global_data.userAgent;
     this.global_data.startTime = Date.now();
-    
+
     // specify view order
-    this.views = [intro, 
+    this.views = [intro,
                   instructionsForcedChoice,
                   practiceForcedChoice,
                   beginForcedChoice,
@@ -22,9 +22,12 @@ exp.init = function() {
                   thanks];
     // initialize counter structure (normally you do not change this)
     this.initializeProcedure();
-    
+
     // prepare information about trials (procedure)
-    this.trial_info = prepareData();
+    // this.trial_info = prepareData();
+
+    // This method uses external files instead.
+    this.trial_info = prepareDataFromCSV("files/practiceTrials.csv", "files/trials.csv");
 };
 
 // create and return an object ('data') where the experiment's info is stored
@@ -44,11 +47,55 @@ var prepareData = function() {
     ];
 
     var data = {
-        'trials': _.shuffle(trials_raw),  // items in data.trials are shuffled randomly upon initialization 
-        'practice_trials': practice_trials, // practice trials occur in the same order for all participants 
+        'trials': _.shuffle(trials_raw),  // items in data.trials are shuffled randomly upon initialization
+        'practice_trials': practice_trials, // practice trials occur in the same order for all participants
         'out': [] // mandatory field to store results in during experimental trials
     };
-    
+
+    return data;
+};
+
+var processTrialsData = function(rows) {
+    var toReturn = [];
+    var headers = rows[0];
+    for (var indexTrial = 1; indexTrial < rows.length; indexTrial++) {
+        var thisTrial = {};
+        for (var indexKey = 0; indexKey < headers.length; indexKey++) {
+            thisTrial[headers[indexKey]] = rows[indexTrial][indexKey];
+        }
+        toReturn.push(thisTrial);
+    }
+    return toReturn;
+};
+
+var prepareDataFromCSV = function(practiceTrialsFile, trialsFile) {
+    var data = {
+        'out': [] // mandatory field to store results in during experimental trials
+    };
+
+    // Need to use such a callback since AJAX is async.
+    var addToContainer = function(container, name, results) {
+        container[name] = results;
+    };
+
+
+    $.ajax({
+        url: practiceTrialsFile,
+        dataType: "text",
+        success: function(file, textStatus, jqXHR) {
+            addToContainer(data, "practice_trials", processTrialsData(CSV.parse(file)));
+        }
+    });
+
+    $.ajax({
+        url: trialsFile,
+        dataType: "text",
+        success: function(file, textStatus, jqXHR) {
+            addToContainer(data, "trials", _.shuffle(processTrialsData(CSV.parse(file))));
+        }
+    });
+
+
     return data;
 };
 
