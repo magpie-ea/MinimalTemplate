5a84d6032183501c92cc71f4e8ee6fb5067ae00f
Michael Franke <the.secret.goldfish@me.com>
2018-04-29 12:27:17 +0200

    moved config_deploy internal info into main.js

--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,22 @@ exp.init = function(){
     this.currentTrialCounter = 0;
     this.currentView = this.findNextView();
 	
-	
+	// user does not (should not) change the following information
+	// checks the config _deploy.deployMethod is MTurk or MTurkSandbox,
+	// sets the submission url to MTukr's servers
+	config_deploy.MTurk_server = config_deploy.deployMethod == "MTurkSandbox" ?
+		"https://workersandbox.mturk.com/mturk/externalSubmit" : // URL for MTurk sandbox
+		config_deploy.deployMethod == 'MTurk' ?
+		"https://www.mturk.com/mturk/externalSubmit" : // URL for live HITs on MTurk
+		""; // blank if deployment is not via MTurk
+	// if the config_deploy.deployMethod is not debug, then liveExperiment is true
+	config_deploy.liveExperiment = config_deploy.deployMethod !== "debug";
+	config_deploy.prolificCode = '9BTAOPQD';
+	config_deploy.is_MTurk = config_deploy.MTurk_server !== "";
+	console.log("deployMethod: " + config_deploy.deployMethod);
+	console.log("live experiment: " + config_deploy.liveExperiment);
+	console.log("runs on MTurk: " + config_deploy.is_MTurk);
+	console.log("MTurk server: " + config_deploy.MTurk_server);
 }
 
 
